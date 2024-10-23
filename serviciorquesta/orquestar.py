967f3e13cb4bb9e4fb718675ce22f5b6dfcd664f
Kevin Velez <kevin.alejandro.velez@correounivalle.edu.co>
2024-10-23 08:43:43 -0500

    update changes

--- serviciorquesta/orquestar.py
+++ serviciorquesta/orquestar.py
@@ -3,20 +3,19 @@ import httpx
 
 app = FastAPI()
 
-# Endpoint en Servicio Orquetador para iniciar la orquestación
 @app.get("/orquestar")
 async def orquestar():
     async with httpx.AsyncClient() as client:
         try:
-            respuesta_a = await client.get("http://servicio-a-service/servicio-a")
+            respuesta_a = await client.get("http://servicio-a-service:80/servicio-a")
             data_a = respuesta_a.json()
-        except httpx.RequestError:
-            data_a = "El servicio A no está disponible"
+        except httpx.RequestError as e:
+            data_a = f"El servicio A no está disponible: {str(e)}"
 
         try:
-            respuesta_b = await client.get("http://servicio-b-service/servicio-b")
+            respuesta_b = await client.get("http://servicio-b-service:80/servicio-b")
             data_b = respuesta_b.json()
-        except httpx.RequestError:
-            data_b = "El servicio B no está disponible"
+        except httpx.RequestError as e:
+            data_b = f"El servicio B no está disponible: {str(e)}"
 
     return {"respuesta_a": data_a, "respuesta_b": data_b}
