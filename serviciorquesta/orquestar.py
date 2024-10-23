33ac10b4bbc02f37325c7ba46ee1de25377b3b48
arnov <arnov@cor3>
2024-10-23 17:17:22 -0500

    se annade el servicio

--- serviciorquesta/orquestar.py
+++ serviciorquesta/orquestar.py
@@ -29,6 +29,12 @@ async def orquestar():
             data_d = respuesta_d.json()
         except httpx.RequestError as e:
             data_d = f"El servicio D no está disponible: {str(e)}"
+
+        try:
+            respuesta_e = await client.get("http://servicio-e-service:3005/servicio-e")
+            data_d = respuesta_e.json()
+        except httpx.RequestError as e:
+            data_e = f"El servicio E no está disponible: {str(e)}"
         
         try:
             respuesta_f = await client.get("http://servicio-f-service:3006/servicio-f")
@@ -36,4 +42,4 @@ async def orquestar():
         except httpx.RequestError as e:
             data_f = f"El servicio F no está disponible: {str(e)}"
 
-    return {"respuesta_a": data_a, "respuesta_b": data_b, "respuesta_c": data_c,"respuesta_d": data_d, "respuesta_f": data_f}
+    return {"respuesta_a": data_a, "respuesta_b": data_b, "respuesta_c": data_c,"respuesta_d": data_d, "respuesta_e": data_e, "respuesta_f": data_f}
